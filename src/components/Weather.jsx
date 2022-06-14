da2fdd92bf688577126259248d223e711ecc01de
Javier Guerra <javier.guerra1001@gmail.com>
2022-06-14 10:03:07 -0700

    Passing test for App component

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Proptypes from 'prop-types';
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
 import styled from 'styled-components';
 import { convertUnix } from '../utils/functions';
 
@@ -17,37 +17,40 @@ function Weather({ weather, showInfo, symbol, activeDiv }) {
   return (
     <div>
       <div className="d-flex">
-        {weather.daily?.map((data, index) => {
-          return (
-            <Weathercard
-              className={index === activeDiv ? 'active-card' : 'card'}
-              onClick={() => showInfo(index)}
-              role="button"
-              tabIndex={0}
-              key={nanoid()}
-            >
-              <div
-                className={index === activeDiv ? 'active' : 'head'}
+        {Object.keys(weather).length !== 0 &&
+          weather.daily.map((data, index) => {
+            return (
+              <Weathercard
+                className={
+                  index === activeDiv ? 'active-card' : 'card'
+                }
+                onClick={() => showInfo(index)}
+                role="button"
+                tabIndex={0}
+                key={Math.floor(Math.random() * 100000000)}
               >
-                <h4>{convertUnix(data.dt)}</h4>
-              </div>
-              <h3>
-                {data.temp.day}
-                {symbol}
-              </h3>
-              <img
-                src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
-                alt={data.weather[0].main}
-              />
-              <p>
-                High: {data.temp.max}
-                {symbol} <br />
-                Low: {data.temp.min}
-                {symbol}
-              </p>
-            </Weathercard>
-          );
-        })}
+                <div
+                  className={index === activeDiv ? 'active' : 'head'}
+                >
+                  <h4>{convertUnix(data.dt)}</h4>
+                </div>
+                <h3>
+                  {data.temp.day}
+                  {symbol}
+                </h3>
+                <img
+                  src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+                  alt={data.weather[0].main}
+                />
+                <p>
+                  High: {data.temp.max}
+                  {symbol} <br />
+                  Low: {data.temp.min}
+                  {symbol}
+                </p>
+              </Weathercard>
+            );
+          })}
       </div>
     </div>
   );
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable import/no-named-as-default-member */
-/* eslint-disable import/no-named-as-default */
 import React, { useEffect, useState } from 'react';
 import '../App.css';
 import Card from '../components/Card';
